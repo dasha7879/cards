560823f9a9b1217fff77ee3af10b529cd2b2d981
dasha7879 <belka7879@gmail.com>
2023-06-08 18:17:34 +0300

    delete Usestrict

--- src/main.tsx
+++ src/main.tsx
@@ -29,11 +29,9 @@ const theme = createTheme({
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
     <ThemeProvider theme={theme}>
     <Provider store={store}>
     <RouterProvider router={router} />
     </Provider>
     </ThemeProvider>
-  </React.StrictMode>,
 )
