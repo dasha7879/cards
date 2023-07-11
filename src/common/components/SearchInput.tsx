ac7fb1f4ea7d542102e21bf865a8bbc9df58ed07
dasha7879 <belka7879@gmail.com>
2023-07-11 16:33:19 +0300

    delete local state to work debounce

--- src/common/components/SearchInput.tsx
+++ src/common/components/SearchInput.tsx
@@ -2,8 +2,7 @@ import Box from "@mui/material/Box"
 import TextField from "@mui/material/TextField"
 import SearchIcon from "@mui/icons-material/Search"
 import InputAdornment from "@mui/material/InputAdornment"
-import { ChangeEvent, FC, useEffect, useState } from "react"
-import { useDebounce } from "../hooks/useDebounce"
+import { ChangeEvent, FC } from "react"
 import { useAppDispatch, useAppSelector } from "../hooks"
 import { dataActions, dataThunks } from "../../features/packs/packs.slice"
 import _ from "lodash"
@@ -16,30 +15,21 @@ type PropsType = {
 export const SearchInput: FC<PropsType> = ({ fullWidth }) => {
   const dispatch = useAppDispatch()
   const params = useAppSelector((state) => state.data.params)
-  const [value, setValue] = useState<string | undefined>(params.packName)
 
   function saveChanges(event: ChangeEvent<HTMLInputElement>) {
     dispatch(dataActions.setParams({ ...params, packName: event.target.value }))
     dispatch(dataThunks.getData({ ...params, packName: event.target.value }))
   }
 
-  useEffect(() => {
-    console.log("kuku")
-    setValue(params.packName)
-  }, [params.packName])
 
   return (
     <Box marginTop={"20px"}>
       <TextField
-        onChange={(event: ChangeEvent<HTMLInputElement>) => {
-          setValue(event.target.value)
-          _.debounce(saveChanges, 500)(event)
-        }}
+        onChange={_.debounce(saveChanges, 500)}
         label="Search"
         fullWidth={fullWidth || false}
         id="fullWidth"
         hiddenLabel
-        value={value ? value : ""}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
